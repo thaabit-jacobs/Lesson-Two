7ea55a7ac17b364e19e60da17850690c386e01e9
Mohammed Thaabit Jacobs <mohammedthaabitjacobs@gmail.com>
2021-01-26 20:30:08 +0200

    j

---
+++ src/Calculator.java
@@ -0,0 +1,29 @@
+public class Calculator {
+
+    public int add(int a, int b){
+
+    }
+
+    public int subtract(int a, int b){
+
+    }
+
+    public int divide(int a, int b){
+
+    }
+
+    public int mutiply(int a, int b){
+
+    }
+
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+
+        System.out.println(cal.add());
+        System.out.println(cal.subtract());
+        System.out.println(cal.mutiply());
+        System.out.println(cal.divide());
+    }
+
+
+}
